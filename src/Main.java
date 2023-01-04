9cf567c5c0e3fde263bd0b8f27ae46d65b745f28
Gulshan <shana_1991@list.ru>
2023-01-04 03:17:17 +0300

    firs commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+      String word="Hello";
+        String word2="Hello" ;
+        System.out.println();
+        System.out.println(word.indexOf("0"));
+
+
+    }
+
+        }
+
+
+
+
+
